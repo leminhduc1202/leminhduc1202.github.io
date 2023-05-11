3c15f2c531cde7e049f9f889297517d8133ef09b
LeMinhDucDN <duc.le@madison-technology.com>
2023-05-11 15:47:15 +0700

    add css and js

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const tabs = document.querySelectorAll('.tab');
+    const tabContents = document.querySelectorAll('.tab-content');
+  
+    tabs.forEach(function(tab) {
+      tab.addEventListener('click', function(event) {
+        event.preventDefault();
+  
+        // Ẩn tất cả nội dung tab-content
+        tabContents.forEach(function(content) {
+          content.style.display = 'none';
+        });
+  
+        // Lấy id của tab-content tương ứng
+        const target = this.getAttribute('href').substring(1);
+        const targetContent = document.getElementById(target);
+  
+        // Hiển thị nội dung tab-content tương ứng
+        targetContent.style.display = 'block';
+      });
+    });
+  });
